deb2a5f21ee941cb93efff47b712ad6e069dceee
Hans Johnson <hans-johnson@uiowa.edu>
2016-09-18 12:46:33 -0500

    COMP: Support cmake package generation
    This series of changes improves the robustness of
    using cmake based tbb in external packages.  It
    provides several improvements for robustness (i.e
    improved error checking with newer cmake versions),
    and enhancements for simplifying external tbb
    inclusion in other projects.
    
    Improve packaging of tbb for cmake
    Follow directions from
    https://cmake.org/cmake/help/v3.0/manual/cmake-packages.7.html#package-configuration-file
    to provide shared library versioning and package configuration for cmake.
    
    This greatly reduces the complexity of including TBB in other cmake based packages.

---
+++ build/tbbConfig.cmake
@@ -0,0 +1,9 @@
+##
+# Generated file to privide information for find_package
+#
+
+include("${CMAKE_CURRENT_LIST_DIR}/tbbTargets.cmake")
+
+## Set old-style find_package names from the tbb directory
+#get_target_property(TBB_INCLUDE_DIRS tbb::tbb INTERFACE_INCLUDE_DIRECTORIES)
+
