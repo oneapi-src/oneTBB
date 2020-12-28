c545252391954b5ca5d8e0cbc3cbce206a512900
Andrey Semashev <andrey.semashev@gmail.com>
2020-12-28 20:33:18 +0300

    Use nanosleep on Linux in atomic_backoff when spinning for a long time.
    This allows to significantly reduce CPU system time while spinning, as
    nanosleep deschedules the thread and allows other threads to progress.
    Unlike sched_yield, nanosleep allows the thread to migrate to other CPUs,
    as well as other threads to migrate to the current CPU, which is what
    allows for more total progress.
    
    See https://github.com/oneapi-src/oneTBB/issues/105. On the test presented
    there, this patch offers to reduce CPU system time by 25-30%. On a real world
    application that test is based on this patch offers ~10% improvement.

--- include/oneapi/tbb/detail/_machine.h
+++ include/oneapi/tbb/detail/_machine.h
@@ -20,6 +20,7 @@
 #include "_config.h"
 #include "_assert.h"
 
+#include <time.h>
 #include <atomic>
 #include <climits>
 #include <cstdint>
@@ -58,6 +59,15 @@ static inline void yield() {
 #else
 using std::this_thread::yield;
 #endif
+
+static inline void long_yield() {
+#if defined(__linux__)
+    struct ::timespec ts = {};
+    ::nanosleep(&ts, 0);
+#else
+    yield();
+#endif
+}
 
 //--------------------------------------------------------------------------------------------------
 // atomic_fence implementation
