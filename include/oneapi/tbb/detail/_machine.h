3e87c259e5847b536478ac2a47346c33a704e7cb
Andrey Semashev <andrey.semashev@gmail.com>
2020-12-28 23:37:46 +0300

    Added optimized x86 atomic_fence for gcc-compatible compilers.
    On x86 (32 and 64-bit) any lock-prefixed instruction provides sequential
    consistency guarantees for atomic operations and is more efficient than
    mfence.
    
    We are choosing a "lock not" on a dummy byte on the stack for the following
    reasons:
    
     - The "not" instruction does not affect flags or clobber any registers.
       The memory operand is presumably accessible through esp/rsp.
     - The dummy byte variable is at the top of the stack, which is likely
       hot in cache.
     - The dummy variable does not alias any other data on the stack, which
       means the "lock not" instruction won't introduce any false data
       dependencies with prior or following instructions.
    
    In order to avoid various sanitizers and valgrind complaining, we have to
    initialize the dummy variable to zero prior to the operation.
    
    Additionally, for memory orders weaker than seq_cst there is no need for
    any special instructions, and we only need a compiler fence. For the relaxed
    memory order we don't need even that.
    
    This optimization is only enabled for gcc up to version 11. In gcc 11 the
    compiler implements a similar optimization for std::atomic_thread_fence.
    Compilers compatible with gcc (namely, clang up to 13 and icc up to 2021.3.0,
    inclusively) identify themselves as gcc < 11 and also benefit from this
    optimization, as they otherwise generate mfence for
    std::atomic_thread_fence(std::memory_order_seq_cst).
    
    Signed-off-by: Andrey Semashev <andrey.semashev@gmail.com>

--- include/oneapi/tbb/detail/_machine.h
+++ include/oneapi/tbb/detail/_machine.h
@@ -84,6 +84,17 @@ using std::this_thread::yield;
 #endif
 
 static inline void atomic_fence(std::memory_order order) {
+#if (__TBB_x86_64 || __TBB_x86_32) && defined(__GNUC__) && __GNUC__ < 11
+    if (order == std::memory_order_seq_cst)
+    {
+        unsigned char dummy = 0u;
+        __asm__ __volatile__ ("lock; notb %0" : "+m" (dummy) :: "memory");
+    }
+    else if (order != std::memory_order_relaxed)
+    {
+        __asm__ __volatile__ ("" ::: "memory");
+    }
+#else
 #if _MSC_VER && (__TBB_x86_64 || __TBB_x86_32)
     if (order == std::memory_order_seq_cst ||
         order == std::memory_order_acq_rel ||
@@ -95,6 +106,7 @@ static inline void atomic_fence(std::memory_order order) {
     }
 #endif /*_MSC_VER && (__TBB_x86_64 || __TBB_x86_32)*/
     std::atomic_thread_fence(order);
+#endif
 }
 
 //--------------------------------------------------------------------------------------------------
