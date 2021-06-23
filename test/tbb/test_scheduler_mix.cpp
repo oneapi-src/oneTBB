46545a52634a155e929161d92826bdd02ed8a54d
Alexei Katranov <alexei.katranov@intel.com>
2021-06-23 10:26:03 +0300

    Use posix_memalign on Unix
    Signed-off-by: Alexei Katranov <alexei.katranov@intel.com>

--- test/tbb/test_scheduler_mix.cpp
+++ test/tbb/test_scheduler_mix.cpp
@@ -81,8 +81,13 @@ thread_local Random::State* Random::mState = nullptr;
 void* aligned_malloc(std::size_t alignment, std::size_t size) {
 #if _WIN32
     return _aligned_malloc(size, alignment);
+#elif __unix__
+    void* ptr{};
+    int res = posix_memalign(&ptr, alignment, size);
+    CHECK(res == 0);
+    return ptr;
 #else
-    return  aligned_alloc(alignment, size);
+    return aligned_alloc(alignment, size);
 #endif
 }
 
