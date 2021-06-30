db14d4edcc3f2b89f0e4b3095d594e998712d4a9
Pablo Romero <pablorcum@gmail.com>
2021-06-30 10:51:37 +0200

    Update src/tbb/misc_ex.cpp
    Co-authored-by: Alex <alexei.katranov@intel.com>

--- src/tbb/misc_ex.cpp
+++ src/tbb/misc_ex.cpp
@@ -65,9 +65,9 @@ static const dynamic_link_descriptor iompLinkTable[] = {
 #endif
 
 static void set_thread_affinity_mask( std::size_t maskSize, const basic_mask_t* threadMask ) {
-#if __unix__
+#if __FreeBSD__ || __NetBSD__ || __OpenBSD__
     if( sched_setaffinity( 0, maskSize, threadMask ) )
-#else /* FreeBSD */
+#else /* __unix__ */
     if( cpuset_setaffinity( CPU_LEVEL_WHICH, CPU_WHICH_TID, -1, maskSize, threadMask ) )
 #endif
         // Here and below the error severity is lowered from critical level
