dc401e3be3252418cacb57583836427c6f5c4ec0
Ilya Isaev <ilya.isaev@intel.com>
2021-07-15 12:47:45 +0300

    Fix test_intrusive_list
    Signed-off-by: Ilya Isaev <ilya.isaev@intel.com>

--- test/tbb/test_intrusive_list.cpp
+++ test/tbb/test_intrusive_list.cpp
@@ -19,7 +19,7 @@
 
 #include "common/test.h"
 #include "common/utils.h"
-#include "../../src/tbb/intrusive_list.h"
+#include "oneapi/tbb/detail/intrusive_list.h"
 
 using tbb::detail::r1::intrusive_list_node;
 
