7ec3cf84081923429cdac26da46eb5aea3fe8d05
Ivan Kochin <iv.kochin@gmail.com>
2021-07-30 15:21:15 +0300

    Fix the HWLOC linking when it was found using pkg-config (#508)
    * Use imported target instead of variables during the HWLOC search using pkg-config
    * Remove redundant code + rephrase the description
    * Fix the indentation
    * Rework testing
    
    Signed-off-by: Kochin Ivan <kochin.ivan@intel.com>
    
    Co-authored-by: Alexey Veprev <alexey.veprev@intel.com>

--- cmake/hwloc_detection.cmake
+++ cmake/hwloc_detection.cmake
@@ -51,9 +51,9 @@ if (NOT HWLOC_TARGET_EXPLICITLY_DEFINED AND
     NOT TBB_DISABLE_HWLOC_AUTOMATIC_SEARCH
 )
     find_package(PkgConfig QUIET)
-    if (PKG_CONFIG_FOUND)
-        pkg_search_module(HWLOC hwloc)
-        if (HWLOC_FOUND)
+    if (PKG_CONFIG_FOUND AND NOT CMAKE_VERSION VERSION_LESS 3.6)
+        pkg_search_module(HWLOC hwloc IMPORTED_TARGET)
+        if (TARGET PkgConfig::HWLOC)
             if (HWLOC_VERSION VERSION_LESS 2)
                 set(TBBBIND_LIBRARY_NAME tbbbind)
             elseif(HWLOC_VERSION VERSION_LESS 2.5)
@@ -64,4 +64,3 @@ if (NOT HWLOC_TARGET_EXPLICITLY_DEFINED AND
         endif()
     endif()
 endif()
-
