90b1b601e150b26a9033147aaa9b0084f567da20
pavelkumbrasev <pavel.kumbrasev@intel.com>
2021-08-23 07:56:55 +0300

    Add error message

--- test/tbb/test_task_arena.cpp
+++ test/tbb/test_task_arena.cpp
@@ -1752,12 +1752,13 @@ struct enqueue_test_helper {
 //! Test for uninitilized arena
 //! \brief \ref requirement \ref error_guessing
 TEST_CASE("Test current_thread_index") {
-    CHECK((tbb::this_task_arena::current_thread_index() == tbb::task_arena::not_initialized));
+    REQUIRE_MESSAGE((tbb::this_task_arena::current_thread_index() == tbb::task_arena::not_initialized), "TBB was initialized state");
 }
 
 // This test requires TBB in an uninitialized state
 //! \brief \ref requirement
 TEST_CASE("task_arena initialize soft limit ignoring affinity mask") {
+    REQUIRE_MESSAGE((tbb::this_task_arena::current_thread_index() == tbb::task_arena::not_initialized), "TBB was initialized state");
     tbb::enumerable_thread_specific<int> ets;
 
     tbb::task_arena arena(int(utils::get_platform_max_threads() * 2));
