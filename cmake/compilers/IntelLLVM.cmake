662948b9d866a96f43fe3980b51e4f2d444bf129
Mishin, Ilya <ilya.mishin@intel.com>
2021-09-13 16:56:07 +0300

    windows ipo
    Signed-off-by: Mishin, Ilya <ilya.mishin@intel.com>

--- cmake/compilers/IntelLLVM.cmake
+++ cmake/compilers/IntelLLVM.cmake
@@ -14,8 +14,6 @@
 
 if (WIN32)
     include(${CMAKE_CURRENT_LIST_DIR}/MSVC.cmake)
-    set(TBB_IPO_COMPILE_FLAGS $<$<NOT:$<CONFIG:Debug>>:/Qipo>)
-    set(TBB_IPO_LINK_FLAGS $<$<NOT:$<CONFIG:Debug>>:/INCREMENTAL:NO>)
 else()
     include(${CMAKE_CURRENT_LIST_DIR}/Clang.cmake)
     set(TBB_IPO_COMPILE_FLAGS $<$<NOT:$<CONFIG:Debug>>:-ipo>)
