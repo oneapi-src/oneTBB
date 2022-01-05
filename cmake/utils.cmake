9595b9699ae6863d1e0cf770a89728eafcaf8845
Christoph Erhardt <github@sicherha.de>
2022-01-05 15:13:32 +0100

    Fix overeager stripping of compile flag
    The existing regex strips all occurrences of the given string from
    `${CMAKE_CXX_FLAGS}`, regardless of whether it is just a substring of a
    flag. For instance, `-Werror=format-security` gets truncated to
    `=format-security`.
    
    The new regex makes sure that only whole words get replaced.
    
    Signed-off-by: Christoph Erhardt <github@sicherha.de>

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -18,7 +18,7 @@ macro(tbb_remove_compile_flag flag)
     set_property(DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR} PROPERTY COMPILE_OPTIONS ${_tbb_compile_options})
     unset(_tbb_compile_options)
     if (CMAKE_CXX_FLAGS)
-        string(REGEX REPLACE ${flag} "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+        string(REGEX REPLACE "(^|[ \t\r\n]+)${flag}($|[ \t\r\n]+)" " " CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
     endif()
 endmacro()
 
