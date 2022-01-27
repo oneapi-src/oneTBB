b7ddbf98d0b9c85f10f2f2be6fed2212941a812a
pavelkumbrasev <pavel.kumbrasev@intel.com>
2022-01-27 15:28:12 +0300

    Add client and permit_manager

---
+++ src/tbb/clients.h
@@ -0,0 +1,59 @@
+/*
+    Copyright (c) 2022 Intel Corporation
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef _TBB_clients_H
+#define _TBB_clients_H
+
+#include "oneapi/tbb/detail/_intrusive_list_node.h"
+
+namespace tbb {
+namespace detail {
+namespace r1 {
+
+class arena;
+
+using mask_type = void*;
+
+class thread_pool_ticket : public d1::intrusive_list_node /* Need for list in thread pool */ {
+public:
+    void apply_mask(mask_type mask);
+    // Interface of communication with thread pool
+    bool try_join() {}
+    void process() {}
+private:
+    arena& m_arena;
+    mask_type m_mask;
+};
+
+// TODO resource_manager_client and thread_pool_client
+class client {
+public:
+    client(arena& a) : m_arena(a) {}
+    // Interface of communication with resource manager
+    virtual void update_allotment() = 0;
+protected:
+    arena& m_arena;
+    thread_pool_ticket m_ticket;
+};
+
+
+} // namespace r1
+} // namespace detail
+} // namespace tbb
+
+
+
+#endif // _TBB_clients_H
---
+++ src/tbb/resource_manager.h
@@ -0,0 +1,58 @@
+/*
+    Copyright (c) 2022 Intel Corporation
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+#ifndef _TBB_permit_manager_H
+#define _TBB_permit_manager_H
+
+#include "oneapi/tbb/detail/_utils.h"
+
+namespace tbb {
+namespace detail {
+namespace r1 {
+
+class arena;
+class client;
+class thread_pool_ticket;
+
+using constraits_type = void*;
+
+class permit_manager : no_copy {
+public:
+    virtual ~permit_manager() {}
+    virtual client* create_client(arena& a, constraits_type* constraits) = 0;
+    virtual void destroy_client(client& c) = 0;
+
+    // ??? Update demand ???
+    // virtual void update_resource_request(unsigned min, unsigned max, client&) = 0;
+    // virtual void release_resource_request(client&) = 0;
+
+    virtual void request_demand(unsigned min, unsigned max, client&) = 0;
+    virtual void release_demand(client&) = 0;
+};
+
+
+class thread_pool : no_copy {
+public:
+    void wake_up(thread_pool_ticket& ticket, unsigned request) {}
+private:
+    // Intrusive_list ticket_list;
+};
+
+} // namespace r1
+} // namespace detail
+} // namespace tbb
+
+#endif // _TBB_permit_manager_H
