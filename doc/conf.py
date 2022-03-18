0a0a5926ee86119e521a5acef9277764344d0afa
Alexandra <alexandra.epanchinzeva@intel.com>
2022-03-18 18:26:28 +0300

    Update conf.py (#810)

--- doc/conf.py
+++ doc/conf.py
@@ -120,18 +120,16 @@ else:
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-if BUILD_TYPE == 'oneapi' or BUILD_TYPE == 'dita':
-    html_theme = 'sphinx_rtd_theme'
-else:
-    html_theme = 'sphinx_book_theme'
-    html_theme_options = {
+
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
     'repository_url': 'https://github.com/oneapi-src/oneTBB',
     'path_to_docs': 'doc/main',
     'use_issues_button': True,
     'use_edit_page_button': True,
     'repository_branch': 'master',
     'extra_footer': '<p align="right"><a href="https://www.intel.com/content/www/us/en/privacy/intel-cookie-notice.html">Cookies</a></p>'
-    }
+}
 
 
 
