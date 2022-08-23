749606004ae5a46a3dd9a08e3003859e55325a1d
kboyarinov <konstantin.boyarinov@intel.com>
2022-08-23 04:06:51 -0500

    Windows warning fix - part 2

--- test/conformance/conformance_parallel_for.cpp
+++ test/conformance/conformance_parallel_for.cpp
@@ -288,8 +288,8 @@ public:
 
     SmartIndex& operator++() { ++real_index; return *this; }
 
-    SmartIndex operator-(const SmartIndex& other) const {
-        return {real_index - other.real_index};
+    std::size_t operator-(const SmartIndex& other) const {
+        return std::size_t(real_index - other.real_index);
     }
 
     SmartIndex operator+(int k) {
