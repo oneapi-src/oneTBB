c45892b6d74af0d9f444c41eda977a41d6432d0a
Valentina Kats <valentina.kats@intel.com>
2022-08-24 20:40:52 +0200

    Update version.h

--- include/oneapi/tbb/version.h
+++ include/oneapi/tbb/version.h
@@ -25,7 +25,7 @@
 // Update version
 #define TBB_VERSION_MINOR 7
 // "Patch" version for custom releases
-#define TBB_VERSION_PATCH 0
+#define TBB_VERSION_PATCH 1
 // Suffix string
 #define __TBB_VERSION_SUFFIX ""
 // Full official version string
@@ -34,7 +34,7 @@
 // OneAPI oneTBB specification version
 #define ONETBB_SPEC_VERSION "1.0"
 // Full interface version
-#define TBB_INTERFACE_VERSION 12070
+#define TBB_INTERFACE_VERSION 12071
 // Major interface version
 #define TBB_INTERFACE_VERSION_MAJOR (TBB_INTERFACE_VERSION/1000)
 // Minor interface version
