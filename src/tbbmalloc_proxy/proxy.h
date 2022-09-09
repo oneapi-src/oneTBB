493774eb57f9c424fc8907d137665e687861ad94
Ismael Luceno <ismael@iodev.co.uk>
2022-09-09 16:18:18 +0200

    Fix build against musl libc
    Probably MALLOC_UNIXLIKE_OVERLOAD_ENABLED only works with glibc, so use
    __GLIBC__ in addition to __linux__ to define it.

--- src/tbbmalloc_proxy/proxy.h
+++ src/tbbmalloc_proxy/proxy.h
@@ -17,7 +17,7 @@
 #ifndef _TBB_malloc_proxy_H_
 #define _TBB_malloc_proxy_H_
 
-#define MALLOC_UNIXLIKE_OVERLOAD_ENABLED __linux__
+#define MALLOC_UNIXLIKE_OVERLOAD_ENABLED (__linux__ && __GLIBC__)
 #define MALLOC_ZONE_OVERLOAD_ENABLED __APPLE__
 
 // MALLOC_UNIXLIKE_OVERLOAD_ENABLED depends on MALLOC_CHECK_RECURSION stuff
