8da1baac35091b0d944929de8ed4f2cd57ec4d46
Ilya Isaev <ilya.isaev@intel.com>
2022-09-26 13:27:57 +0200

    Update python/tbb/test.py
    Signed-off-by: Isaev, Ilya <ilya.isaev@intel.com>

--- python/tbb/test.py
+++ python/tbb/test.py
@@ -103,7 +103,7 @@ def test(arg=None):
             say("Good. Got expected timeout exception.")
         else:
             assert False, "Expected exception !"
-        spin_flag = False
+        spin_flag = False # unlock threads
 
     ### Test copy/pasted from multiprocessing
     pool = Pool(4)  # start worker threads
