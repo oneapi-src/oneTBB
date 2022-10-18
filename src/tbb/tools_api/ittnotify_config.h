e8666c0b9cd64ebedfbaf3032412983deebc9e43
Dirk Mueller <dmueller@suse.com>
2022-10-18 10:34:09 +0200

    Add s390x, hppa and riscv64 architecture detection (#917)
    * Fix build failure for s390x and hppa architectures.
    
    Signed-off-by: Mo Zhou <cdluminate@gmail.com>
    
    * Add riscv64 architecture detection
    
    Signed-off-by: Dirk Müller <dirk@dmllr.de>
    
    Signed-off-by: Mo Zhou <cdluminate@gmail.com>
    Signed-off-by: Dirk Müller <dirk@dmllr.de>
    Co-authored-by: Mo Zhou <cdluminate@gmail.com>

--- src/tbb/tools_api/ittnotify_config.h
+++ src/tbb/tools_api/ittnotify_config.h
@@ -167,6 +167,18 @@
 #  define ITT_ARCH_LOONGARCH64  7
 #endif /* ITT_ARCH_LOONGARCH64 */
 
+#ifndef ITT_ARCH_S390X
+#  define ITT_ARCH_S390X  8
+#endif /* ITT_ARCH_S390X */
+
+#ifndef ITT_ARCH_HPPA
+#  define ITT_ARCH_HPPA  9
+#endif /* ITT_ARCH_HPPA */
+
+#ifndef ITT_ARCH_RISCV64
+#  define ITT_ARCH_RISCV64  10
+#endif /* ITT_ARCH_RISCV64 */
+
 #ifndef ITT_ARCH
 #  if defined _M_IX86 || defined __i386__
 #    define ITT_ARCH ITT_ARCH_IA32
@@ -182,6 +194,12 @@
 #    define ITT_ARCH ITT_ARCH_PPC64
 #  elif defined __loongarch__
 #    define ITT_ARCH ITT_ARCH_LOONGARCH64
+#  elif defined __s390__ || defined __s390x__
+#    define ITT_ARCH ITT_ARCH_S390X
+#  elif defined __hppa__
+#    define ITT_ARCH ITT_ARCH_HPPA
+#  elif defined __riscv && __riscv_xlen == 64
+#    define ITT_ARCH ITT_ARCH_RISCV64
 #  endif
 #endif
 
