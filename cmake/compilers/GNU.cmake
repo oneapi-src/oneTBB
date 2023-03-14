f91f55f25ad121a39c5de3a1cacbd6c56ed7313c
sarathnandu <sarath.nandu.ramachandran.nair@intel.com>
2023-03-14 15:10:57 -0500

    Fix test build failure in Mips for debug mode. (#1042)
    * Removed the "-fPIC -shared" flag added by cmake by default.
    
    Signed-off-by: sarathnandu <sarath.nandu.ramachandran.nair@intel.com>

--- cmake/compilers/GNU.cmake
+++ cmake/compilers/GNU.cmake
@@ -72,7 +72,7 @@ endif ()
 # Gnu flags to prevent compiler from optimizing out security checks
 set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -fno-strict-overflow -fno-delete-null-pointer-checks -fwrapv)
 set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -Wformat -Wformat-security -Werror=format-security
-	                                                  -fstack-protector-strong -fPIC -shared )
+    -fstack-protector-strong )
 set(TBB_LIB_LINK_FLAGS ${TBB_LIB_LINK_FLAGS} -Wl,-z,relro,-z,now,-z,noexecstack)
 set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} $<$<NOT:$<CONFIG:Debug>>:-D_FORTIFY_SOURCE=2> )
 
