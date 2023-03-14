3ab9cde06b3340106de637b87aee703f0c8ee5b2
sarathnandu <sarath.nandu.ramachandran.nair@intel.com>
2023-03-13 19:06:32 -0500

    Updates based on review comments.

--- cmake/compilers/GNU.cmake
+++ cmake/compilers/GNU.cmake
@@ -76,10 +76,6 @@ set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -Wformat -Wformat-secur
 set(TBB_LIB_LINK_FLAGS ${TBB_LIB_LINK_FLAGS} -Wl,-z,relro,-z,now,-z,noexecstack)
 set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} $<$<NOT:$<CONFIG:Debug>>:-D_FORTIFY_SOURCE=2> )
 
-# Text relocation for MIPS doesn't work for heavy tests with too many symbols in debug.
-if (NOT "${CMAKE_SYSTEM_PROCESSOR}" MATCHES "mips")
-    set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -fPIC -shared)
-endif()
 
 # TBB malloc settings
 set(TBBMALLOC_LIB_COMPILE_FLAGS -fno-rtti -fno-exceptions)
