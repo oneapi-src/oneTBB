a9a0d46b01ac363a6905735ef46b97d223844ed2
pavelkumbrasev <pavel.kumbrasev@intel.com>
2023-06-12 12:58:59 +0100

    Fix current_thread_index for masterless arena(1,0)
    Signed-off-by: pavelkumbrasev <pavel.kumbrasev@intel.com>

--- src/tbb/arena.h
+++ src/tbb/arena.h
@@ -312,8 +312,8 @@ public:
     static arena& allocate_arena( market& m, unsigned num_slots, unsigned num_reserved_slots,
                                   unsigned priority_level );
 
-    static int unsigned num_arena_slots ( unsigned num_slots ) {
-        return max(2u, num_slots);
+    static int unsigned num_arena_slots ( unsigned num_slots, unsigned num_reserved_slots ) {
+        return num_reserved_slots == 0 ? num_slots : max(2u, num_slots);
     }
 
     static int allocation_size ( unsigned num_slots ) {
--- test/tbb/test_task_arena.cpp
+++ test/tbb/test_task_arena.cpp
@@ -1979,3 +1979,22 @@ TEST_CASE("is_inside_task in arena::execute") {
     });
 }
 #endif //__TBB_PREVIEW_TASK_GROUP_EXTENSIONS
+
+//! \brief \ref interface \ref requirement \ref regression
+TEST_CASE("worker threads occupy slots in correct range") {
+    std::vector<tbb::task_arena> arenas(42);
+    for (auto& arena : arenas) {
+        arena.initialize(1, 0);
+    }
+
+    tbb::task_group tg;
+    for (auto& arena : arenas) {
+        arena.execute([&] {
+            tg.run([] {
+                CHECK(tbb::this_task_arena::current_thread_index() == 0);
+            });
+        });
+    }
+
+    tg.wait();
+}
