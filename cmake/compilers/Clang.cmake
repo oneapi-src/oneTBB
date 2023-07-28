277402d5de51d3817a7f38abefa3e7464441a0e9
JhaShweta1 <shweta.jha@intel.com>
2023-07-28 15:20:10 -0500

    clean up

--- cmake/compilers/Clang.cmake
+++ cmake/compilers/Clang.cmake
@@ -58,7 +58,10 @@ endif()
 # Clang flags to prevent compiler from optimizing out security checks
 set(TBB_COMMON_COMPILE_FLAGS ${TBB_COMMON_COMPILE_FLAGS} -Wformat -Wformat-security -Werror=format-security -fPIC $<$<NOT:$<IF:${EMSCRIPTEN},1,0>>:-fstack-protector-strong>)
 
-set(TBB_LIB_LINK_FLAGS ${TBB_LIB_LINK_FLAGS} -Wl,-z,relro,-z,now)
+# -z switch is not supported on MacOS
+if (NOT APPLE)
+    set(TBB_LIB_LINK_FLAGS ${TBB_LIB_LINK_FLAGS} -Wl,-z,relro,-z,now)
+endif()
 
 set(TBB_COMMON_LINK_LIBS ${CMAKE_DL_LIBS})
 
