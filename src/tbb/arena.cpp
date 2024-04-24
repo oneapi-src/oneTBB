92c8529ff49f6bf6067ba67bbf07b8dd2d47433c
Dmitri Mokhov <dmitri.n.mokhov@intel.com>
2024-04-24 16:50:56 -0500

    Add task arena worker block time (#1352)
    * Add `threading_control::is_any_other_client_active()` to verify if other arenas are requesting workers.
    * Add hybrid CPU detection at initialization.
    * On non-hybrid CPUs, make workers block for 1ms before leaving the arena, unless
      - more work becomes available in the arena (-> unblock and stay in the arena);
      - other arenas are requesting workers (-> unblock and leave the arena immediately).
    
    Signed-off-by: Dmitri Mokhov <dmitri.n.mokhov@intel.com>
    Signed-off-by: pavelkumbrasev <pavel.kumbrasev@intel.com>

--- src/tbb/arena.cpp
+++ src/tbb/arena.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2005-2023 Intel Corporation
+    Copyright (c) 2005-2024 Intel Corporation
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
@@ -396,7 +396,7 @@ bool arena::is_top_priority() const {
 }
 
 bool arena::try_join() {
-    if (num_workers_active() < my_num_workers_allotted.load(std::memory_order_relaxed)) {
+    if (is_joinable()) {
         my_references += arena::ref_worker;
         return true;
     }
