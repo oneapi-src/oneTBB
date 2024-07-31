16b3c7cd925d8406e682238069918a128ae09d7e
pavelkumbrasev <pavel.kumbrasev@intel.com>
2024-07-31 12:03:37 +0100

    Add CF linker flag
    Signed-off-by: pavelkumbrasev <pavel.kumbrasev@intel.com>

--- cmake/compilers/MSVC.cmake
+++ cmake/compilers/MSVC.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2020-2021 Intel Corporation
+# Copyright (c) 2020-2024 Intel Corporation
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -70,3 +70,4 @@ if (TBB_WINDOWS_DRIVER)
 endif()
 
 set(TBB_OPENMP_FLAG /openmp)
+set(TBB_LIB_LINK_FLAGS ${TBB_LIB_LINK_FLAGS} /guard:cf)
