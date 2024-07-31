e99e983d4d73496dcc2d873a01b5854491999e68
sarathnandu <sarath.nandu.ramachandran.nair@intel.com>
2024-07-31 17:36:46 -0500

    Apply llvm-format to measurements.hpp

--- examples/common/utility/measurements.hpp
+++ examples/common/utility/measurements.hpp
@@ -18,6 +18,7 @@
 #define TBB_measurements_H
 
 #include <cmath>
+
 #include "oneapi/tbb/tick_count.h"
 
 namespace utility {
@@ -25,40 +26,33 @@ namespace utility {
 // utility class to aid relative error measurement of samples
 class measurements {
 public:
-    measurements() {
-        clear();
-    }
-    void clear() {
-        _secPerFrame.clear();
-    }
-    void start() {
-        _startTime = oneapi::tbb::tick_count::now();
-    }
-    void stop() {
-        _endTime = oneapi::tbb::tick_count::now();
-        auto count = _endTime - _startTime;
-        _secPerFrame.push_back(count.seconds());
-    }
-    double computeRelError() {
-        auto averageTimePerFrame = 
-           std::accumulate(_secPerFrame.begin(), _secPerFrame.end(), 0.0) /
-                           _secPerFrame.size();
-        std::vector<double> diff(_secPerFrame.size());
-        std::transform(_secPerFrame.begin(),
-                       _secPerFrame.end(),
-                       diff.begin(),
-                       [averageTimePerFrame](double x) {
-                           return (x - averageTimePerFrame);
-                       });
-        double sumOfSquareDiff = std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
-        double stdDev = std::sqrt(sumOfSquareDiff / _secPerFrame.size());
-        double relError = 100 * (stdDev / averageTimePerFrame);
-        return relError;
-    }
+  measurements() { clear(); }
+  void clear() { _secPerFrame.clear(); }
+  void start() { _startTime = oneapi::tbb::tick_count::now(); }
+  void stop() {
+    _endTime = oneapi::tbb::tick_count::now();
+    auto count = _endTime - _startTime;
+    _secPerFrame.push_back(count.seconds());
+  }
+  double computeRelError() {
+    auto averageTimePerFrame =
+        std::accumulate(_secPerFrame.begin(), _secPerFrame.end(), 0.0) /
+        _secPerFrame.size();
+    std::vector<double> diff(_secPerFrame.size());
+    std::transform(
+        _secPerFrame.begin(), _secPerFrame.end(), diff.begin(),
+        [averageTimePerFrame](double x) { return (x - averageTimePerFrame); });
+    double sumOfSquareDiff =
+        std::inner_product(diff.begin(), diff.end(), diff.begin(), 0.0);
+    double stdDev = std::sqrt(sumOfSquareDiff / _secPerFrame.size());
+    double relError = 100 * (stdDev / averageTimePerFrame);
+    return relError;
+  }
+
 private:
-    std::vector<double> _secPerFrame;
-    oneapi::tbb::tick_count _startTime;
-    oneapi::tbb::tick_count _endTime;
+  std::vector<double> _secPerFrame;
+  oneapi::tbb::tick_count _startTime;
+  oneapi::tbb::tick_count _endTime;
 };
 
 } // namespace utility
