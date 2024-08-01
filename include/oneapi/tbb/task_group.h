db69fca64ee033dae78fef9d60d19d6898f669eb
kboyarinov <konstantin.boyarinov@intel.com>
2024-08-01 05:13:56 -0500

    Fix incorrect alignment with master

--- include/oneapi/tbb/task_group.h
+++ include/oneapi/tbb/task_group.h
@@ -551,11 +551,7 @@ public:
     task_group_status wait() {
         bool cancellation_status = false;
         try_call([&] {
-<<<<<<< HEAD
-            d1::wait( m_wait_vertex.get_context(), context());
-=======
             d1::wait(m_wait_vertex.get_context(), context());
->>>>>>> origin/master
         }).on_completion([&] {
             // TODO: the reset method is not thread-safe. Ensure the correct behavior.
             cancellation_status = m_context.is_group_execution_cancelled();
