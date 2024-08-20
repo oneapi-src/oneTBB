c062204fd73b21c80badd20aff644cdfaaf24a5a
Konstantin Boyarinov <konstantin.boyarinov@intel.com>
2024-08-20 13:04:44 +0300

    Update test/tbb/test_join_node_preview.cpp
    Co-authored-by: Mike Voss <michaelj.voss@intel.com>

--- test/tbb/test_join_node_preview.cpp
+++ test/tbb/test_join_node_preview.cpp
@@ -314,6 +314,7 @@ void test_try_put_and_wait_key_matching(Body... body) {
         }
 
         CHECK_MESSAGE(processed_items[check_index++] == wait_message, "Unexpected wait_message processing");
+        CHECK_MESSAGE(check_index == processed_items.size(), "Unexpected number of messages");
 
         g.wait_for_all();
 
