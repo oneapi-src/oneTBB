ae6b4ab981f8990ab3d855381fff5d54cb35e7ee
James Reinders <james@jamesreinders.com>
2024-08-26 18:41:02 -0700

    updated one copyright

--- new_examples/exception/exception_catch2.cpp
+++ new_examples/exception/exception_catch2.cpp
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2019 Intel Corporation
+Copyright (C) 2024 Intel Corporation
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"),
