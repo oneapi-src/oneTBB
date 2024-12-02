bcab38faf20676f919ffbc6d5b9b6e06057fa746
James Reinders <james@jamesreinders.com>
2024-12-02 15:35:48 -0800

    revised figure 3-1 code

--- new_examples/containers/pop_danger.cpp
+++ new_examples/containers/pop_danger.cpp
@@ -16,25 +16,54 @@
 
 #include <iostream>
 #include <queue>
+#include <tbb/parallel_invoke.h>
 
 int main() {
-  int sum (0);
-  int item;
+  int sum0(0), sum1(0); 
 
   std::priority_queue<int> myPQ;
  
   for(int i=0; i<10001; i+=1) {
     myPQ.push(i);
   }
- 
-  while( !myPQ.empty() ) {
-    sum += myPQ.top();
-    myPQ.pop();
-  }
+  
+  tbb::parallel_invoke( 
+    [&]() { 
+      while( !myPQ.empty() ) {
+	sum0 += myPQ.top();
+	myPQ.pop();
+      }
+    }, 
+    [&]() { 
+      while( !myPQ.empty() ) {
+	sum1 += myPQ.top();
+	myPQ.pop();
+      }
+    }); 
 
-  // prints "total: 50005000" (for 0,10001,1)
+  // if correct (which it is not) this
+  // would print "total: 50005000" (for 0,10001,1)
   std::cout << "total: "
-	    << sum << '\n';
-
+	    << sum0+sum1 << '\n';
   return 0;
 }
+
+//
+// Sample outputs:
+// % ./pop_danger
+// total: 128379594
+// % ./pop_danger
+// total: 124432912
+// % ./pop_danger
+// total: 107697942
+// % ./pop_danger
+// total: 50005000
+// % ./pop_danger
+// total: 115224669
+// % ./pop_danger
+// total: 146790135
+// % ./pop_danger
+// total: 130683763
+// % ./pop_danger
+// total: 126960607
+//
--- new_examples/containers/pop_danger_fixed.cpp
+++ new_examples/containers/pop_danger_fixed.cpp
@@ -1,12 +1,9 @@
 /*
     Copyright (c) 2024 Intel Corporation
-
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at
-
         http://www.apache.org/licenses/LICENSE-2.0
-
     Unless required by applicable law or agreed to in writing, software
     distributed under the License is distributed on an "AS IS" BASIS,
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,26 +12,32 @@
 */
 
 #include <iostream>
-
 #include <tbb/concurrent_priority_queue.h>
+#include <tbb/parallel_invoke.h>
 #include <tbb/parallel_for.h>
 
-
-int main() {
-  int sum (0);
-  int item = 0;
-
-  tbb::concurrent_priority_queue<int> myPQ;
- 
-  tbb::parallel_for(0,10001,1,
-		    [&](size_t i){myPQ.push(i);} );
- 
-  while( myPQ.try_pop(item) ) 
-    sum += item;
+int main() { 
+  int sum0(0), sum1(0); 
+  
+  tbb::concurrent_priority_queue<int> myPQ; 
+  
+  tbb::parallel_for(0,10001,1, 
+        [&](size_t i){ myPQ.push(i); } ); 
+  
+  tbb::parallel_invoke( 
+    [&]() { 
+      int item = 0; 
+      while( myPQ.try_pop(item) ) 
+        sum0 += item; 
+    }, 
+    [&]() { 
+      int item = 0; 
+      while( myPQ.try_pop(item) ) 
+        sum1 += item; 
+    }); 
 
   // prints "total: 50005000" (for 0,10001,1)
   std::cout << "total: "
-	    << sum << '\n';
-
+	    << sum0+sum1 << '\n';
   return 0;
 }
